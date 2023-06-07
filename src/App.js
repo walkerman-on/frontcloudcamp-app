c87920081d15ba119ffb26c60e13098a893ae0e7
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2023-06-08 00:19:50 +0300

    Making first step in app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <section>
+      <div class="user-img">
+        <span class="first-text">АИ</span>
+      </div>
+      </section>
     </div>
   );
 }
